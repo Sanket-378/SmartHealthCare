548819cff03450300166a5455ab2590ad35d66ef
Sanket-378 <sanketkhatkale07@gmail.com>
2026-05-16 23:05:25 +0530

    Initialize setup of project structure

---
+++ backend/src/main/java/com/healthcare/medicompare/MedicompareApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.medicompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicompareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/healthcare/medicompare/MedicompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.medicompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
